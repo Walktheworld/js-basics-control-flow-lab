ffb55c32094e8297d51d8662c3203a5df1c3f14a
joe <walktheworld@me.com>
2021-04-07 14:04:35 -0600

    commit tree 46241abd66166eb1d3352b745e400cfaf896977c

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  let greeting
+
+  if (distance <= 400) {
+    return greeting = 'This one is on me!';
+  }  
+  else if (distance > 2000 && distance < 2500) {
+    return greeting = 'I will gladly take your thirty bucks.';
+  }
+  else if (distance > 2500) {
+  return greeting = 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  const response = city === 'NYC' ? "Ok, sounds good." : "No go.";
+
+  return response
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip){
+  
+  let gratitude;
+
+  switch (tip) {
+    case 'generous':
+       gratitude = 'Thank you so much.';
+        break;
+
+    case 'not as generous':
+        gratitude = 'Thank you.';
+        break;
+
+    case 'thanks for everything':
+        gratitude = 'Bye.';
+        break;
+      }
+    return gratitude
+  }
